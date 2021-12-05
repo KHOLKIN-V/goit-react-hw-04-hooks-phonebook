135d5009efbf283e23d423ddde16dc94331400f3
KHOLKIN-V <kholkinva@yandex.ru>
2021-12-06 02:24:46 +0400

    add localstorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
 import AddContactForm from './components/AddContactForm/AddContactForm'
@@ -8,6 +8,17 @@ import ContactBook from './components/ContactBook/ContactBook';
 export default function App() {
     const [contacts, setContacts] = useState([]);
     const [filter, setFilter] = useState('');
+
+    useEffect(() => {
+      localStorage.setItem("contacts", JSON.stringify(contacts))
+    }, [contacts]);
+
+    useEffect(() => {
+      const parsContacts = JSON.parse(localStorage.getItem("contacts"));
+      if (parsContacts) {
+        setContacts(parsContacts);
+      }
+    },[])
 
     const formSubmit = (data) => {
 
